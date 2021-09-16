90c157b10483c4816a2f1f70a97c70fdedd6e5d5
Andrei <rusandrei95@gmail.com>
2021-09-16 11:28:53 +0300

    Tema Session 03 - greeting

---
+++ src/tema/sci/greeting/Main.java
@@ -0,0 +1,18 @@
+package tema.sci.greeting;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String name = sc.nextLine();
+        if (name.isEmpty()){
+            System.out.println("Hello, stranger.");
+        }else{
+            System.out.println("Hello, " + name + "!");
+        }
+    }
+
+}
