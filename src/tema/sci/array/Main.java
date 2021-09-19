0fe6fc46af0a0f1743ac153b1f2b152a39f19db6
Andrei <rusandrei95@gmail.com>
2021-09-19 19:57:58 +0300

    Tema array .

--- src/tema/sci/array/Main.java
+++ src/tema/sci/array/Main.java
@@ -2,5 +2,13 @@ package tema.sci.array;
 
 public class Main {
 
+    public static void main(String[] args) {
 
+        String[] myArray = {"I ", "am ", "learning ", "java", "."};
+        String mySentence = "";
+        for (String s : myArray) {
+            mySentence += s;
+        }
+        System.out.println(mySentence);
+    }
 }
