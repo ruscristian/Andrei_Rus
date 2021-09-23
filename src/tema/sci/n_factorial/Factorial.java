2fbae07d9490488e5ca6881a839291c9ef6c4cbd
Andrei <rusandrei95@gmail.com>
2021-09-23 10:00:59 +0300

    Tema Session 04 - factorial

---
+++ src/tema/sci/n_factorial/Factorial.java
@@ -0,0 +1,19 @@
+package tema.sci.n_factorial;
+
+import java.util.Scanner;
+
+public class Factorial {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        double number = scanner.nextInt();
+        double j = number;
+        for(double i = j - 1; i < number && i > 0; i-- ){
+
+            number = j * i;
+            j = number;
+        }
+        System.out.println("Factorial number is: " + number);
+    }
+}
