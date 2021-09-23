14560fba772e5430379dae158a300c592c067757
Andrei <rusandrei95@gmail.com>
2021-09-23 11:19:40 +0300

    Tema Session 04 - factorial.**

--- src/tema/sci/n_factorial/Factorial.java
+++ src/tema/sci/n_factorial/Factorial.java
@@ -13,14 +13,18 @@ public class Factorial {
     }
 
     private static long normalFormula(long number) {
-        long j = number;
-        for(long i = j - 1; i < number && i > 0; i-- ){
+        if (number == 0){
+            return 1;
+        } else {
+            long j = number;
+            for (long i = j - 1; i < number && i > 0; i--) {
 
-            number = j * i;
-            j = number;
+                number = j * i;
+                j = number;
+            }
+
+            return number;
         }
-
-        return number;
     }
 
     private static long recursiveFormula(long n){
