c3c82b563c514bd8dfacf928b7b245f9ae504a1c
Andrei <rusandrei95@gmail.com>
2021-10-14 18:05:04 +0300

    Library Catalog.

--- src/main/java/tema/sci/library_catalog/ArtAlbum.java
+++ src/main/java/tema/sci/library_catalog/ArtAlbum.java
@@ -8,25 +8,6 @@ public class ArtAlbum extends Book{
 
     private String quality;
 
-//    private List<ArtAlbum> badQualityAlbumList = new LinkedList<>();
-//    private List<ArtAlbum> goodQualityAlbumList = new LinkedList<>();
-//    private List<ArtAlbum> excellentQualityAlbumList = new LinkedList<>();
-//    private List<ArtAlbum> undefinedQualityAlbumList = new LinkedList<>();
-//
-//
-//
-//
-//    public void addInListByQuality(String quality){
-//        if(quality == "bad"){
-//            badQualityAlbumList.add( this);
-//        } else if (quality == "good"){
-//            goodQualityAlbumList.add(this);
-//        }else if (quality == "excellent"){
-//            excellentQualityAlbumList.add(this);
-//        }else {
-//            undefinedQualityAlbumList.add(this);
-//        }
-//    }
 
     public ArtAlbum(String name, String pages, String quality) {
         super(name, pages);
